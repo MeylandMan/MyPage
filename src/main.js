8ea8a23b0be4f26404cc6652ba1fd05d49747e32
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-02-06 22:12:12 +0100

    Created scene

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(72, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
 

d0b348600699989c3c63007167af153be680388f
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-02-06 22:50:37 +0100

    Rendering the cube
    Made it on half the dimension to know if the thing is rendering above the header

--- src/main.js
+++ src/main.js
@@ -2,13 +2,22 @@ import './style.css'
 
 import * as THREE from 'three';
 
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+
 // Set the scene
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(72, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize( window.innerWidth, window.innerHeight );
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
+
+renderer.setSize( window.innerWidth/2, window.innerHeight/2);
 document.body.appendChild( renderer.domElement );
 
 // Drawing cube
@@ -17,4 +26,12 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+
+camera.position.z = 2;
+
+// Animate process
+
+function animate() {
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );

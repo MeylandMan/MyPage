f13e4853bcbfe88ec272dab5e55fc8a1ebf82da0
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-02-07 02:19:06 +0100

    Added a Glowing Welcome Text

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import './style.css'
+import './style.css';
 
-import * as THREE from 'three'
+import * as THREE from 'three';
 
-import WebGL from 'three/addons/capabilities/WebGL.js'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import { ThreeMFLoader } from 'three/examples/jsm/Addons.js'
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { ceilPowerOfTwo } from 'three/src/math/MathUtils.js';
+//import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+//import { ThreeMFLoader } from 'three/examples/jsm/Addons.js'
 
 
 // Set the scene
@@ -16,46 +17,45 @@ const renderer = new THREE.WebGLRenderer({
 	canvas: document.querySelector('#bg'),
 });
 
-const controls = new OrbitControls( camera, renderer.domElement )
-const loader = new GLTFLoader()
+const controls = new OrbitControls( camera, renderer.domElement );
 
 
-document.body.appendChild( renderer.domElement )
+document.body.appendChild( renderer.domElement );
 
-//Directional light
-const pointLight = new THREE.PointLight(0xffffff, 100)
-pointLight.position.set(5, 5, 5)
-const ambiantLight = new THREE.AmbientLight(0xffffff)
+//Adding lights
+const pointLight = new THREE.PointLight(0xffffff, 100);
+pointLight.position.set(5, 5, 5);
+const ambiantLight = new THREE.AmbientLight(0xffffff);
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
+const lightHelper = new THREE.PointLightHelper(pointLight);
 
-const gridHelp = new THREE.GridHelper(200, 50)
+const gridHelp = new THREE.GridHelper(200, 50);
 
-scene.add( pointLight,  ambiantLight, lightHelper, gridHelp)
+scene.add( pointLight,  ambiantLight, lightHelper, gridHelp);
 
-// Drawing Torus
-const geometry = new THREE.TorusGeometry( 5, 1, 10, 50 )
-const material = new THREE.MeshStandardMaterial( { color: 0xFF6347} )
-const torus = new THREE.Mesh( geometry, material )
+// Torus
+const geometry = new THREE.TorusGeometry( 5, 1, 10, 50 );
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347} );
+const torus = new THREE.Mesh( geometry, material );
 
 scene.add( torus );
 
 camera.position.z = 5;
 
-loader.load("../Classic Gun/fps_guns_4k_-_pistol_2 (1).glb", function ( gltf ) {
 
-	const model = gltf.scene;
-    model.scale.set(10, 10, 10)
-	model.position.set(0, -1, 0)
+// Stars 
+function AddStar() {
+	const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+	const material = new THREE.MeshStandardMaterial(0xffffff);
+	const star = new THREE.Mesh(geometry, material);
 
-	scene.add( model )
+	const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 )) ;
 
-}, undefined, function ( error ) {
+	star.position.set(x, y, z);
+	scene.add( star );
+}
 
-	console.error( error )
-
-} )
-
+Array(500).fill().forEach(AddStar)
 
 // Animate process
 if(!WebGL.isWebGL2Available()) {
@@ -72,6 +72,8 @@ renderer.setAnimationLoop( () => {
 	
 	torus.rotation.x += 0.01
 	torus.rotation.y += 0.01
+	controls.update()
+
 	renderer.setPixelRatio( window.devicePixelRatio )
 	renderer.setSize( window.innerWidth, window.innerHeight);
 

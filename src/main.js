5ac9b8737c106be18f56d2dc9466bc16171cd3e1
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-02-07 02:24:38 +0100

    Removed Star function
    The skybox is doing what I want actually so that's pretty cool

--- src/main.js
+++ src/main.js
@@ -42,20 +42,9 @@ scene.add( torus );
 
 camera.position.z = 5;
 
-
-// Stars 
-function AddStar() {
-	const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-	const material = new THREE.MeshStandardMaterial(0xffffff);
-	const star = new THREE.Mesh(geometry, material);
-
-	const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 )) ;
-
-	star.position.set(x, y, z);
-	scene.add( star );
-}
-
-Array(500).fill().forEach(AddStar)
+// Add Texture to scene
+const sceneTexture = new THREE.TextureLoader().load('../OuterSpace-Skybox.jpg');
+scene.background = sceneTexture;
 
 // Animate process
 if(!WebGL.isWebGL2Available()) {
